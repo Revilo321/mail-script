ead574e1923d87aa2cd2a326e699e543ab1b2e97
Revilo321 <oliverwii@live.dk>
2023-10-05 13:25:41 +0200

    edited email_message to text_message

--- mail_alert.py
+++ mail_alert.py
@@ -8,7 +8,7 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 def main():
-    EMAIL_MESSAGE = os.environ.get('EMAIL_MESSAGE')
+    TEXT_MESSAGE = os.environ.get('TEXT_MESSAGE')
     try:
         email_handler = EmailHandler()
         sms_handler = SMSHandler()
@@ -16,7 +16,7 @@ def main():
         email_ids = email_handler.check_email_for_sender()
         if email_ids:
             logging.info(f"Found {len(email_ids)} new emails from target sender.")
-            sms_handler.send_sms(f"{EMAIL_MESSAGE}")
+            sms_handler.send_sms(f"{TEXT_MESSAGE}")
             email_handler.mark_as_read(email_ids)
         else:
             logging.info("No new emails found from target sender.")
