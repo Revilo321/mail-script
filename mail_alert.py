72d3298e9f3cb5870edac1a6a173656d54b04d8d
Revilo321 <oliverwii@live.dk>
2023-10-05 13:23:25 +0200

    added more envs to limit personal info

--- mail_alert.py
+++ mail_alert.py
@@ -1,10 +1,14 @@
+import os
 import logging
 from email_handler import EmailHandler
 from sms_handler import SMSHandler
+from dotenv import load_dotenv
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
 def main():
+    EMAIL_MESSAGE = os.environ.get('EMAIL_MESSAGE')
     try:
         email_handler = EmailHandler()
         sms_handler = SMSHandler()
@@ -12,7 +16,7 @@ def main():
         email_ids = email_handler.check_email_for_sender()
         if email_ids:
             logging.info(f"Found {len(email_ids)} new emails from target sender.")
-            """ sms_handler.send_sms(f"You have {len(email_ids)} new emails from target sender!") """
+            sms_handler.send_sms(f"{EMAIL_MESSAGE}")
             email_handler.mark_as_read(email_ids)
         else:
             logging.info("No new emails found from target sender.")
